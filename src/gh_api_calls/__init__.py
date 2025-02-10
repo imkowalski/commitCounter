4773091f539e3d3aa8ef896d8e3d868028c0a440
Michal Kowalski <kowal@outlook.dk>
2025-02-10 14:15:18 +0100

    changed the commit counter to actually work

--- src/gh_api_calls/__init__.py
+++ src/gh_api_calls/__init__.py
@@ -1,9 +1,15 @@
 import requests
 import subprocess
-import json
+import json, re
+
+def extract_page_number(link_header):
+    match = re.search(r'.*"next".*page=(\d{1,4}).*"last".*', link_header)
+    return match.group(1) if match else None
 
 def get_commits(user, repo,api_key):
-    url = f'https://api.github.com/repos/{user}/{repo}/commits'
+    url = f'https://api.github.com/repos/{user}/{repo}/commits?per_page=1'
     
-    res = json.loads(subprocess.Popen(f"curl {url} -H \"Authorization: Bearer {api_key}\"", shell=True, stdout=subprocess.PIPE).stdout.read())
-    return len(res)
+    res = subprocess.Popen(f"curl -I -k {url} -H \"Authorization: Bearer {api_key}\"", shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')   
+    page_number = extract_page_number(res)
+
+    return int(page_number)
